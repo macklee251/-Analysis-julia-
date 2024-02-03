13f3c4ced8b57e43c99dcf0cd68a4132df970686
mack lee <114943402+macklee251@users.noreply.github.com>
2024-02-03 19:13:49 -0300

    .

---
+++ notes/optimization.jl
@@ -0,0 +1,12 @@
+#=
+    JuMP high-level interface that allows us to use a commom syntax 
+    to access many different optimization solvers like GLPK (Linear solver) 
+    and Ipopt (Nonlinear solver).
+=#
+  
+#=
+    The Plots is similar to the JuMP, it is a package that serves 
+    as plotting frontend that can access several different plotting 
+    packages, like GR and PyPlot,  to use as backends.
+=#
+
