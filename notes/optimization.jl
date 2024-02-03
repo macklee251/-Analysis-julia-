889f0acab26a2558d42bf8fde8f66432cf1fa842
mack lee <114943402+macklee251@users.noreply.github.com>
2024-02-03 19:25:37 -0300

    .

--- notes/optimization.jl
+++ notes/optimization.jl
@@ -10,3 +10,29 @@
     packages, like GR and PyPlot,  to use as backends.
 =#
 
+#=
+@article{DunningHuchetteLubin2017,
+author = {Iain Dunning
+and Joey Huchette and Miles Lubin},
+title = {JuMP: A ModeLing Language for Mathematical Optimization},
+journal = {SIAM Review},
+voLume = ｛59｝，
+number=128，
+pages = ｛295-320｝，
+year = (2017),
+dor = {10.1137/15M1020575},
+=#
+
+# import Pkg; Pkg.add("JuMP"), Pkg.add("GLPK") ,Pkg.add("Ipopt"), Pkg.add("Plots");   
+using JuMP, GLPK, Ipopt, Plots
+
+model = Model(GLPK. Optimizer)
+@variable(model, x >= 0)
+@variable(model, y >= 0)
+@constraint(model, 6x + 8y >= 100)
+@constraint(model, 7x + 12y >= 120)
+@objective(model, Min, 12x + 20y)
+optimize!(model)
+@show value(x) ;
+@show value(y) ;
+@show objective_value(model);
